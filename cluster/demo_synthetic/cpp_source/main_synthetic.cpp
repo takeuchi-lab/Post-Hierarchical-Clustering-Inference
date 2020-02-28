a2bef9bd2ba557675e4d63c155608c6ace9aadd5
kenpi-608 <z8bafsiuiu517javky9j@docomo.ne.jp>
2020-02-28 18:42:29 +0900

    Fix synthetic experiment

--- cluster/demo_synthetic/cpp_source/main_synthetic.cpp
+++ cluster/demo_synthetic/cpp_source/main_synthetic.cpp
@@ -7,6 +7,16 @@
 
 using namespace Eigen;
 
+// 小数点数を指定してstring型に変える
+template <typename ... Args>
+std::string format(const std::string& fmt, Args ... args )
+{
+    size_t len = std::snprintf( nullptr, 0, fmt.c_str(), args ... );
+    std::vector<char> buf(len + 1);
+    std::snprintf(&buf[0], len + 1, fmt.c_str(), args ... );
+    return std::string(&buf[0], &buf[0] + len);
+}
+
 int main(int argc, char* argv[]){
     if (argc >= 6){
         int epo = std::stod(argv[1]);
@@ -14,26 +24,38 @@ int main(int argc, char* argv[]){
         int d = std::stod(argv[3]);
         int step = std::stod(argv[4]);
 	    int threads = std::stod(argv[5]);
-	    // std::cout << "epo: " << epo << std::endl;
-	    // std::cout << "n: " << n << std::endl;
-	    // std::cout << "d: " << d << std::endl;
-	    // std::cout << "step: " << step << std::endl;
-        // std::cout << "threads: " << threads << std::endl;
+        double mu = std::stod(argv[6]);
 
         // データ作成
     	// std::chrono::system_clock::time_point  start, end; // 型は auto で可
         // start = std::chrono::system_clock::now();
+        int half_n = n / 2;
         for (int epoch = 0; epoch < epo; epoch++){
             // std::cout << "------epoch: " << epoch << "------" << std::endl; 
             std::mt19937 seed_gen(epoch); 
             std::default_random_engine engine(seed_gen());
-            std::normal_distribution<> dist(0.0, 1.0);
-            MatrixXd data(n, d);
-            for (int i = 0; i < n; i++){
+            std::normal_distribution<> dist_mu(mu, 1.0);
+            MatrixXd data_mu(half_n, d);
+            for (int i = 0; i < half_n; i++){
                 for (int j = 0; j < d; j++){
-                    data(i, j) = dist(engine);
+                    data_mu(i, j) = dist_mu(engine);
                 }
-            }      
+            }
+            std::normal_distribution<> dist(0, 1.0);
+            MatrixXd data_zero(half_n, d);
+            for (int i = 0; i < half_n; i++){
+                for (int j = 0; j < d; j++){
+                    data_zero(i, j) = dist(engine);
+                }
+            }
+            MatrixXd data(n, d);
+            for (int i = 0; i < half_n; i++){
+                data.row(i) = data_mu.row(i);
+            }
+            for (int i = half_n; i < n; i++){
+                data.row(i) = data_zero.row(i - half_n);
+            }
+
             // クラスタリング    
             // std::cout << "------------------Clustering------------------" << std::endl;
             std::vector< std::vector<double> > result;
@@ -46,15 +68,21 @@ int main(int argc, char* argv[]){
             MatrixXd Sigma = MatrixXd::Identity(n, n);
             double xi = 1;
             std::tie(chi2, final_interval) = PCI_cluster_ward_step(data, cluster_head_vec, cluster_next_vec, cluster_tail_vec, selected_c, Sigma, xi, step, threads);
+            
             // 統計量 ファイル出力
-            std::string fname1 = "stat/test_stat_d_epoch" + std::to_string(epo) + "_step" + std::to_string(step) + "_n" +  std::to_string(n) + "_d" + std::to_string(d) + ".csv";
+            std::string fname1;
+            if (mu > 0.0) fname1 = "stat/test_stat_d_epoch" + std::to_string(epo) + "_step" + std::to_string(step) + "_n" +  std::to_string(n) + "_d" + std::to_string(d) + "_mu" + format("%.1f" ,mu) + ".csv";
+            else fname1 = "stat/test_stat_d_epoch" + std::to_string(epo) + "_step" + std::to_string(step) + "_n" +  std::to_string(n) + "_d" + std::to_string(d) + ".csv";
+            
             std::ofstream Test_Stat;
             Test_Stat.open(fname1, std::ios::app);
             Test_Stat << chi2 << "," << d << std::endl;
             Test_Stat.close();
 
             // 区間 ファイル出力
-            std::string fname2 = "interval/final_interval_epoch"+ std::to_string(epo) + "_step" + std::to_string(step) + "_n" +  std::to_string(n) + "_d" + std::to_string(d) + ".csv";
+            std::string fname2;
+            if (mu > 0.0) fname2 = "interval/final_interval_epoch"+ std::to_string(epo) + "_step" + std::to_string(step) + "_n" +  std::to_string(n) + "_d" + std::to_string(d) + "_mu" + format("%.1f" ,mu) + ".csv";
+            else fname2 = "interval/final_interval_epoch"+ std::to_string(epo) + "_step" + std::to_string(step) + "_n" +  std::to_string(n) + "_d" + std::to_string(d) + ".csv";
             std::ofstream Interval(fname2, std::ios::app);
             for (int i = 0; i < final_interval.size(); i++){
                 if (i < final_interval.size() - 1){
